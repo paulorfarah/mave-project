5be380aa87e93b29937e7f07ea0ff4bee9490f25
Paulo Roberto Farah <paulorfarah@gmail.com>
2021-12-31 11:48:02 -0300

    first commit

---
+++ src/main/java/App.java
@@ -0,0 +1,26 @@
+public class App {
+	public String getGreeting() {
+        System.out.println("start");
+        stringBuilder();
+        try {
+        	Thread.sleep(1 * 1000);
+        	} catch (InterruptedException ie) {
+        	Thread.currentThread().interrupt();
+        	}
+        return "Hello World!";
+    }
+	
+	public void stringBuilder() {
+		System.out.println("stringBuilder");
+		StringBuilder sb = new StringBuilder();
+		sb.append(new char[1_000_000]);
+		for( int i=0; i< 300000; i++) {
+			sb.append(12345);
+			sb.delete(0, 5);
+		}
+	}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test public void testAppHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+    
+    @Test public void testDelay13seconds() {
+    	try {
+			Thread.sleep(13000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			
+		}
+    }
+    
+    @Test public void testDelay30seconds() {
+    	try {
+    		Thread.sleep(30000);
+    	} catch(InterruptedException e) {
+    		e.printStackTrace();
+    	}
+    	testDelay13seconds();
+    }
+
+}
