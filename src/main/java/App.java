4bdbbafaeac270a87d25f647ee39c25889d1467c
Paulo Roberto Farah <paulorfarah@gmail.com>
2022-01-11 11:54:07 -0300

    added new function to test execution time

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
 public class App {
 	public String getGreeting() {
         System.out.println("start");
+        countSetBits(123456789);
+        
         try {
     		Thread.sleep(30000);
     	} catch(InterruptedException e) {
@@ -19,12 +21,22 @@ public class App {
 		System.out.println("stringBuilder");
 		StringBuilder sb = new StringBuilder();
 		sb.append(new char[1_000_000]);
-		for( int i=0; i< 300000; i++) {
+		for( int i=0; i< 300000000; i++) {
 			sb.append(12345);
 			sb.delete(0, 5);
 		}
 	}
 
+	static int countSetBits(int n)
+    {
+        int count = 0;
+        while (n > 0) {
+            count += n & 1;
+            n >>= 1;
+        }
+        return count;
+    }
+	
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
