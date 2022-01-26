f07a8a880ab962572ff8fb013958afd55e4f282a
Paulo Roberto Farah <paulorfarah@gmail.com>
2022-01-25 21:48:37 -0300

    added new method to test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,8 @@
+import java.math.BigDecimal;
+import java.util.Random;
+
 public class App {
+
 	public String getGreeting() {
         System.out.println("start");
         countSetBits(1);
@@ -36,6 +40,30 @@ public class App {
         }
         return count;
     }
+	
+	public void cpuUsage() {
+		Random random = new Random();
+		BigDecimal bigDecimal = BigDecimal.ZERO;
+		int i = 0;
+        while (i < 1000) {
+            double value = Math.atan(Math.sqrt(Math.pow(random.nextInt(10), random.nextDouble())));
+            if (random.nextInt(10) > 5) {
+                bigDecimal = bigDecimal.add(BigDecimal.valueOf(value));
+            } else {
+                bigDecimal = bigDecimal.subtract(BigDecimal.valueOf(value));
+            }
+            i++;
+        }
+	}
+	
+	public void justPrintMessage() {
+		System.out.println("testing 1 2 3...");
+		System.out.println("testing 1 2 3...");
+		System.out.println("testing 1 2 3...");
+		System.out.println("testing 1 2 3...");
+		System.out.println("testing 1 2 3...");
+	}
+	
 	
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
